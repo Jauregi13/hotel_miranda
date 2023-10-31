1d826bc6eeb0d88df01ddc90aaedce5fdda4e09e
jauregi13 <jon.j1305@gmail.com>
2023-10-31 01:59:31 +0100

    header del mobile añadido

---
+++ header.php
@@ -0,0 +1,25 @@
+
+
+
+<header class="header">
+
+    <div class="header__content">
+
+        <div class="header__content__hamburger-icon">&#9776;</div>
+        <div class="header__content__logo">
+            <div class="header__content__logo__icon">H</div>
+            <div class="header__content__logo__title">HOTEL MIRANDA</div>
+        </div>
+        <div class="header__content__icons">
+            <div class="header__content__icons__account">
+                <img src="./img/accountIcon.svg" alt="account"/>
+            </div>
+            <div class="header__content__icons__search">
+                <img src="./img/searchIcon.svg" />
+            </div>
+        </div>
+        
+
+    </div>
+
+</header>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="./css/header.css">
+        <link rel="stylesheet" href="./css/styles.css">
+        <title>Home</title>
+    </head>
+    <body>
+        <?php 
+            include 'header.php'
+        ?>
+        
+    </body>
+</html>
