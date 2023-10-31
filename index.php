516a1bc2aedf97f54aea319b9b8fd39fb6ef4d34
jauregi13 <jon.j1305@gmail.com>
2023-10-31 13:36:46 +0100

    banner del home diseñado

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="./css/header.css">
         <link rel="stylesheet" href="./css/styles.css">
         <title>Home</title>
     </head>
@@ -10,6 +9,29 @@
         <?php 
             include 'header.php'
         ?>
+
+        <div class="home-content">
+
+            <div class="home-content__banner-home">
+
+                <div class="home-content__banner-home__content">
+
+                    <p class="home-content__banner-home__content__text">THE ULTIMATE LUXURY EXPERIENCE</p>
+                    <h1 class="home-content__banner-home__content__title">The Perfect Base For You</h1>
+
+                    <div class="home-content__banner-home__content__buttons">
+
+                        <button class="home-content__banner-home__content__buttons__tour">TAKE A TOUR</button>
+                        <button class="home-content__banner-home__content__buttons__learn">LEARN MORE</button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+
+
         
     </body>
 </html>
